410c73dd24c3b6ea849a55604f67d7c2ed6f0dc2
Daniel Levelev <levelev@gmail.com>
2018-03-24 18:38:52 +0100

    removed jquery

--- chat.js
+++ chat.js
@@ -10,6 +10,5 @@ window.CRISP_WEBSITE_ID = "97c1be79-e705-4e31-be7c-e6eb68067d8c";
 //     d.getElementsByTagName("head")[0].appendChild(s);
 // })();
 
-$.getScript("https://client.crisp.chat/l.js")
 
 console.log('ended script');
