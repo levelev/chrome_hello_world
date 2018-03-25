eef9c95cae9cf14da2b20c36c5962534a5db3026
Daniel Levelev <levelev@gmail.com>
2018-03-25 11:43:48 +0200

    first working mvp

--- chat.js
+++ chat.js
@@ -1,14 +1,23 @@
 console.log('started script');
 
-window.$crisp = [];
-window.CRISP_WEBSITE_ID = "97c1be79-e705-4e31-be7c-e6eb68067d8c";
-// (function() {
-//     d = document;
-//     s = d.createElement("script");
-//     s.src = "https://client.crisp.chat/l.js";
-//     s.async = 1;
-//     d.getElementsByTagName("head")[0].appendChild(s);
-// })();
+
+
+(function() {
+    d = document;
+    s = d.createElement("script");
+    s.src = "https://client.crisp.chat/l.js";
+    s.async = 1;
+    d.getElementsByTagName("head")[0].appendChild(s);
+
+    s2 = d.createElement("script");
+    s2.text = `
+      window.$crisp = [];
+      window.CRISP_WEBSITE_ID = "97c1be79-e705-4e31-be7c-e6eb68067d8c";
+    `;
+    d.getElementsByTagName("head")[0].appendChild(s2);
+    d.getElementsByTagName("head")[0].appendChild(s);
+
+})();
 
 
 console.log('ended script');
